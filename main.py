2858f19cf62af8cd2643e57cd6bd6b6cb3317fdc
zhuangruiying <zhuanglinlin0321@gmail.com>
2025-11-10 18:11:50 +0800

    add

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,7 +19,3 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8004)
